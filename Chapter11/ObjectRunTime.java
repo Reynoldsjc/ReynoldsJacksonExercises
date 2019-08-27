853747487e9e8c6523c3f2eaeee48ced4924ad2d
Jackson Reynolds <jackson.c.reynolds22@gmail.com>
2019-08-27 12:49:48 -0600

    add new file

--- Chapter11/ObjectRunTime.java
+++ Chapter11/ObjectRunTime.java
@@ -1,5 +1,10 @@
 public class ObjectRunTime {
     public static void main (String[] args) {
-        
+        String str;
+        String Marty;
+        str = new String("Hello World");
+        Marty = new String("Mountain Vista");
+        System.out.println( str );
+        System.out.println( Marty );
     }
 }
