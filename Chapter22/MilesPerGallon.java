9426eaae420d57ddc2a7dde87038d027f1c32851
Jackson Reynolds <jackson.c.reynolds22@gmail.com>
2019-09-22 19:20:31 -0600

    finish Chapter22

---
+++ Chapter22/MilesPerGallon.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class MilesPerGallon
+{
+  public static void main (String[] args)
+  {
+    System.out.println("Miles Per Gallon Program");
+    int i = 0;
+
+    while (i == 0){
+      Scanner scan = new Scanner(System.in);
+      System.out.println("What's your initial miles?");
+      int initialmiles = scan.nextInt();
+
+      if (initialmiles >= 0){
+        scan = new Scanner(System.in);
+        System.out.println("What's your final miles??");
+        int finalmiles = scan.nextInt();
+
+        scan = new Scanner(System.in);
+        System.out.println("How many gallons did that take??");
+        int gallons = scan.nextInt();
+
+        double mpg = (finalmiles - initialmiles) / gallons;
+
+        System.out.println("Ayeeeeee your mpg is " + mpg);
+
+      } else{
+        System.out.println("bye");
+        i += 1;
+      }
+    }
+
+  }
+}
